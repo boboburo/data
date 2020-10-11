5e016dfdfe8c4317992d5428c4d58c1dc0754c7e
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-11 18:06:10 +0100

    add path for debugging

--- practice/grab-data.sh
+++ practice/grab-data.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo $DIR
+
 curl "https://data.nasa.gov/resource/y77d-th95.json" | \
     sqlite-utils insert practice.db meteorites - | \
     echo here are the list of files
