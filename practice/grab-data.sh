4ff89421bf8bc8b67d7d4b1e455d298d2ef68b97
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-11 18:10:12 +0100

    new path

--- practice/grab-data.sh
+++ practice/grab-data.sh
@@ -7,4 +7,4 @@ echo $DIR
 curl "https://data.nasa.gov/resource/y77d-th95.json" | \
     sqlite-utils insert practice.db meteorites - | \
     echo here are the list of files
-    ls -atlh practice/ 
+    ls -atlh
