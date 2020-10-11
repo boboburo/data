0585ea19b73c51520e8dd95bd2fef2b79065e20f
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-11 17:55:06 +0100

    List files in directory

--- practice/grab-data.sh
+++ practice/grab-data.sh
@@ -1,4 +1,4 @@
 curl "https://data.nasa.gov/resource/y77d-th95.json" | \
     sqlite-utils insert practice.db meteorites - | \
     echo here are the list of files
-    ls -atlh
+    ls -atlh practice/ 
