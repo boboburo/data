3ed5803cbe925d21b26985a1dbc3afc4c9bd5290
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-11 17:50:42 +0100

    Script pulls data

--- practice/grab-data.sh
+++ practice/grab-data.sh
@@ -1,1 +1,4 @@
-echo "first github action"
+curl "https://data.nasa.gov/resource/y77d-th95.json" | \
+    sqlite-utils insert practice.db meteorites - | \
+    echo here are the list of files
+    ls -atlh
