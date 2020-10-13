4833477c8d794221fb1a35afdeabbc0436097584
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-13 17:16:57 +0100

    Tidy up file

--- covid-ireland/pull-data.sh
+++ covid-ireland/pull-data.sh
@@ -1,2 +1,11 @@
+#!/bin/sh
+
 curl https://opendata.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.geojson \
-    | geojson-to-sqlite covid-ireland.db dailycases - --pk OBJECTID
+    | geojson-to-sqlite covid-ireland.db dailycases_geo - --pk OBJECTID
+
+sqlite3 covid-ireland.db <<'END_SQL'
+.timeout 2000
+CREATE TABLE dailycases AS SELECT OBJECTID, ORIGID,CountyName,PopulationCensus16,TimeStamp,
+ConfirmedCovidCases,PopulationProportionCovidCases FROM dailycases_geo;
+DROP TABLE IF EXISTS dailycases_geo;
+END_SQL
