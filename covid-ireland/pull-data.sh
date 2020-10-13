db72ae32de9f32c9622e201909cc8305d42e6bd1
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-13 20:16:59 +0100

    Fix SQL to delete table if exists

--- covid-ireland/pull-data.sh
+++ covid-ireland/pull-data.sh
@@ -5,6 +5,7 @@ curl https://opendata.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.geo
 
 sqlite3 covid-ireland.db <<'END_SQL'
 .timeout 2000
+DROP TABLE IF EXISTS dailycases;
 CREATE TABLE dailycases AS SELECT OBJECTID, ORIGID,CountyName,PopulationCensus16,TimeStamp,
 ConfirmedCovidCases,PopulationProportionCovidCases FROM dailycases_geo;
 DROP TABLE IF EXISTS dailycases_geo;
