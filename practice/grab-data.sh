0a2775b3942d19ff6e132e7372dc6af9b619dee7
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-13 14:06:33 +0100

    Remove the file print out

--- practice/grab-data.sh
+++ practice/grab-data.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 echo $DIR
 
 curl "https://data.nasa.gov/resource/y77d-th95.json" | \
-    sqlite-utils insert practice.db meteorites - | \
-    echo here are the list of files
-    ls -atlh
+sqlite-utils insert practice_with_ga.sqlite meteorites - 
