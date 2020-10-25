d11a4a7872bf92724f9e8b1d9b15d7a086ab27b8
iBrianCarter <bcarter_ie@yahoo.co.uk>
2020-10-25 09:32:36 +0000

    Change csv url for data pull. Other was unstable. Add manual trigger for workflow.

--- covid-ireland/pull-data.sh
+++ covid-ireland/pull-data.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
-curl https://opendata.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.geojson \
-    | geojson-to-sqlite covid-ireland.db dailycases_geo - --pk OBJECTID
+#curl "https://opendata.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.geojson" | \
+#geojson-to-sqlite covid-ireland.db dailycases_geo - --pk OBJECTID 
+
+curl "https://opendata-geohive.hub.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D" | \
+sqlite-utils insert --csv covid-ireland.db dailycases_new - --pk OBJECTID 
 
 sqlite3 covid-ireland.db <<'END_SQL'
 .timeout 2000
 DROP TABLE IF EXISTS dailycases;
 CREATE TABLE dailycases AS SELECT OBJECTID, ORIGID,CountyName,PopulationCensus16,TimeStamp,
-ConfirmedCovidCases,PopulationProportionCovidCases FROM dailycases_geo;
+ConfirmedCovidCases,PopulationProportionCovidCases FROM dailycases_new;
 DROP TABLE IF EXISTS dailycases_geo;
 END_SQL
+
+
+
+---WITH tablelen AS (SELECT COUNT(*) FROM dailycases)
+---DROP TABLE IF tablelen > 7000 dailycases;
+---CREATE TABLE dailycases AS SELECT OBJECTID, ORIGID,CountyName,PopulationCensus16,TimeStamp,
+---ConfirmedCovidCases,PopulationProportionCovidCases FROM dailycases_geo;
+---DROP TABLE IF EXISTS dailycases_geo;
+END_SQL
+
+
